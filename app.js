d956a01f0221d11c74914c23844277eafadc8828
Thanapon Sathirathiwat <tsathira@uci.edu>
2017-10-21 03:18:13 -0700

    Adding decrement karma points feature.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var tmi = require('tmi.js');
 var dict = {};
 var botPassword = "oauth:vi0vhkpbl7x0frm3dxnpuzgwp88hxi";
 var rankLimit = 3;
+var karmaUser = "";
 
 var options = {
     options:{ 
@@ -34,6 +35,7 @@ client.on("chat", function (channel, userstate, message, self) {
 
     var botCmd = message.toLowerCase();
 
+    // Feature: show my karma points
     if(botCmd == "!karma me"){
         var karmaUser = userstate['display-name'];
         if(dict[karmaUser]){
@@ -46,6 +48,7 @@ client.on("chat", function (channel, userstate, message, self) {
         }
     }
 
+    // Feature: show the top people who have the most karma points
     if(botCmd == "!karma") {
         var items = Object.keys(dict).map(function(key) {
             return [key, dict[key]];
@@ -70,29 +73,58 @@ client.on("chat", function (channel, userstate, message, self) {
         return;
     }
 
-    var karmaUser = message.substring(message.lastIndexOf("@")+1,message.indexOf("+"));
+    karmaUser = message.substring(message.lastIndexOf("@")+1,message.indexOf("+"));
+    // Feature: detect non karma bot commands
     if (karmaUser.length == 0) {
         console.log(`${message} - not a karma message`);
         return;
     }
-
-    if (karmaUser == userstate["display-name"]) {
-        console.log(`${karmaUser} attempting to give karma to himself`);
-        client.action(chatroom, "Don't be a smartass.")
-        return;
-    }
-    
-    if(dict[karmaUser]){
-        dict[karmaUser] += 1; 
-        console.log(dict);
-        if (dict[karmaUser]%5 === 0){
-            client.action(chatroom, karmaUser +" has accumulated "+ dict[karmaUser] +" karma points, Great job!"); 
-        }else{
-            client.action(chatroom, karmaUser +` now has ${dict[karmaUser]} karma point.`); 
+    if (karmaUser != "@") {
+        if (karmaUser == userstate["display-name"]) {
+            console.log(`${karmaUser} attempting to give karma to himself`);
+            client.action(chatroom, "Don't be a smartass.")
+            return;
         }
-    } else {
-        dict[karmaUser] = 1;
-        client.action(chatroom, karmaUser +" has his/her first karma point!"); 
-        console.log(dict);
+        
+        // Feature: add karma to other users , announce when user acquired a point or meet some points treshold
+        if(dict[karmaUser]){
+            dict[karmaUser] += 1; 
+            console.log(dict);
+            if (dict[karmaUser] != 0 && dict[karmaUser]%5 === 0){
+                client.action(chatroom, karmaUser +" has accumulated "+ dict[karmaUser] +" karma points, Great job!");
+                return; 
+            }else{
+                client.action(chatroom, karmaUser +` now has ${dict[karmaUser]} karma point.`); 
+                return;
+            }
+        } else {
+            dict[karmaUser] = 1;
+            client.action(chatroom, karmaUser +" has his/her first karma point!"); 
+            console.log(dict);
+            return;
+        }
     }
+
+    // Feature: deduct karma to other users , announce when user acquired a point or meet some points treshold
+    karmaUser = message.substring(message.lastIndexOf("@")+1,message.indexOf("-"));
+    if (karmaUser != "@") {
+        if (karmaUser == userstate["display-name"]) {
+            console.log(`${karmaUser} attempting to deduct his karma`);
+            client.action(chatroom, "Don't try reducing your own karma.")
+            return;
+        }
+        if(dict[karmaUser]){
+            dict[karmaUser] -= 1; 
+            console.log(dict);
+            client.action(chatroom, karmaUser +` now has ${dict[karmaUser]} karma point.`); 
+            return;
+        }else{
+            dict[karmaUser] = -1;
+            client.action(chatroom, karmaUser +` now has ${dict[karmaUser]} karma point.`); 
+            return;
+        }   
+    }
+
+    console.log("User failed to increment / decrement karma.")
+    client.action(chatroom, `Sorry don't know what you're trying to do.`);
 });
